a8061c9f72130908c6f08cce73e2e00c13744a72
Joe Lee <zvmboni@gmail.com>
2022-06-19 11:32:19 -0700

    added proto navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import './NavBar.css'
+import { useNavigate } from 'react-router-dom'
 
 function NavBar() {
+  const navigate = useNavigate();
+
+  const handleHomeClick = () => {
+    navigate('/')
+  }
+
+  const handleProfileClick = () => {
+    navigate('/profile')
+  }
 
   return (
     <div>
-        <img id='nerdherdLogo' src='/nerdherd_logo.png' alt='nerdherd_logo'></img>
+        <img id='nerdherdLogo' src='/nerdherd_logo.png' alt='nerdherd_logo' onClick={handleHomeClick}/>
+        <img src='https://static.vecteezy.com/system/resources/previews/002/318/271/original/user-profile-icon-free-vector.jpg' alt='profile' onClick={handleProfileClick}/>
     </div>
   )
 }
